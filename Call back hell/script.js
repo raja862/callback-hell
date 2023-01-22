869af055068e04f108686573fab7278a47b6c0d2
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2023-01-22 11:17:37 +0530

    Add files via upload

---
+++ Call back hell/script.js
@@ -0,0 +1,46 @@
+function out(){
+    document.querySelector('.timer').innerHTML=`${"Happy Independence Day"}`
+}setTimeout(out,11000)
+
+function ten(out){
+    document.querySelector('.timer').innerHTML=`${10}`
+}setTimeout(ten,10000)
+function nine(ten){
+    document.querySelector('.timer').innerHTML=`${9}`
+}setTimeout(nine,9000)
+function eight(nine){
+    document.querySelector('.timer').innerHTML=`${8}`
+}setTimeout(eight,8000)
+function seven(eight){
+    document.querySelector('.timer').innerHTML=`${7}`
+}setTimeout(seven,7000)
+function six(seven){
+    document.querySelector('.timer').innerHTML=`${6}`
+}setTimeout(six,6000)
+function five(six){
+    document.querySelector('.timer').innerHTML=`${5}`
+}setTimeout(five,5000)
+
+function four(five){
+    document.querySelector('.timer').innerHTML=`${4}`
+}setTimeout(four,4000)
+
+
+function three(four){
+    //console.log(1)
+    document.querySelector('.timer').innerHTML=`${3}`
+}setTimeout(three,3000)
+
+
+function two(three){
+    
+    //console.log(1)
+    document.querySelector('.timer').innerHTML=`${2}`
+}setTimeout(two,2000)
+
+
+function one(two){
+    
+    //console.log(1)
+    document.querySelector('.timer').innerHTML=`${1}`
+}setTimeout(one,1000)
